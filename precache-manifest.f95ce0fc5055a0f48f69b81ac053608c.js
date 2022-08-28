bbe1dd4f9987acbe72ec71b158ed3521b65d5951
RYU <huyaye@naver.com>
2022-08-28 11:04:42 +0900

    Updates

---
+++ precache-manifest.f95ce0fc5055a0f48f69b81ac053608c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b80b6901be35da7fe052",
+    "url": "/nwitter/static/css/main.17fae33f.chunk.css"
+  },
+  {
+    "revision": "b80b6901be35da7fe052",
+    "url": "/nwitter/static/js/main.b80b6901.chunk.js"
+  },
+  {
+    "revision": "8427b010e45104253faf",
+    "url": "/nwitter/static/js/1.8427b010.chunk.js"
+  },
+  {
+    "revision": "09f57dde8cca147bf378",
+    "url": "/nwitter/static/js/runtime~main.09f57dde.js"
+  },
+  {
+    "revision": "8b2308ca11c4bc64df79117659d17f2e",
+    "url": "/nwitter/index.html"
+  }
+];
